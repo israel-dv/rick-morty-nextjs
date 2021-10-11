99cf6b984414b954022bc5d641a2ec15a5481645
israel-dv <se.israel.mc@gmail.com>
2021-10-11 11:09:05 -0500

    Type episode was implemented

--- src/components/CardShimmers/index.tsx
+++ src/components/CardShimmers/index.tsx
@@ -7,7 +7,9 @@ const CardShimmers: React.FC<CardShimmersProps> = ({
 }: CardShimmersProps) => {
   return (
     <div className="animate-pulse flex h-28 border-2 border-gray-700 rounded-lg items-center px-4 bg-gray-800 cursor-pointer shadow-lg">
-      <div className="rounded-full w-24 h-24 bg-gray-200 border-2 border-white cursor-pointer" />
+      {type === 'character' && (
+        <div className="rounded-full w-24 h-24 bg-gray-200 border-2 border-white cursor-pointer" />
+      )}
       <div className="flex flex-col ml-2">
         <div className="bg-white h-5 w-16 rounded-md"></div>
         <div className="bg-white h-4 w-28 rounded-md mt-2"></div>
