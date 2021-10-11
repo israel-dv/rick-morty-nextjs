98eba74f2bf94d98dd50f2e93b001c94b9f6ae57
israel-dv <se.israel.mc@gmail.com>
2021-10-10 20:35:22 -0500

    Get CHaracters from API

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -1,22 +1,120 @@
 import type { NextPage } from 'next'
+import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
+import Link from 'next/link'
 
 import HeadTitle from 'components/HeadTitle'
 import Layout from 'components/Layout'
 import ROUTES from 'utils/constants/routes'
+import Paginator from 'components/Paginator'
+import useCounter from 'utils/hooks/useCounter'
+import useLocalStorage from 'utils/hooks/useLocalStorage'
 import { charactersQuery } from 'api/characters/charctersQuery'
+import { CharactersProps } from 'utils/interfaces/characters'
+
+interface Info {
+  count: number
+  pages: number
+}
+
+interface CharactersPageProps {
+  results: Array<CharactersProps>
+  info: Info
+}
+
+const INIT_INFO = {
+  pages: 0,
+  count: 0,
+}
+
+const INIT_PAGE = 1
 
 const Characters: NextPage = () => {
-  const { loading, data } = useQuery(charactersQuery)
+  const [characters, setCharacters] = useState(
+    (): CharactersPageProps => ({
+      results: [],
+      info: INIT_INFO,
+    }),
+  )
 
-  console.log(loading, data)
+  const { value: page, setLocalStorage } = useLocalStorage({
+    key: 'page',
+    initialValue: INIT_PAGE,
+  })
+
+  const { counter, increment, decrement } = useCounter({
+    initialCounter: page,
+  })
+
+  const { data, loading } = useQuery(charactersQuery(page))
+
+  useEffect(() => {
+    if (data && !loading) {
+      const { results, info } = !!data && data.characters
+      setCharacters({ results, info })
+    }
+  }, [data, loading])
+
+  useEffect(() => {
+    setLocalStorage(counter)
+  }, [counter])
 
   return (
     <Layout>
       <HeadTitle title={ROUTES.characters.label} />
-      {ROUTES.characters.label}
+      <div className="flex flex-col w-full h-full">
+        <div className="flex w-full justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-3xl text-white">
+              {ROUTES.characters.label}
+            </span>
+            <span className="text-sm text-white">
+              {loading
+                ? 'Loading'
+                : `Page ${page} of ${characters?.info?.pages}`}
+            </span>
+          </div>
+          <Paginator
+            lastPage={characters.info.pages}
+            currentPage={page}
+            onClickBack={decrement}
+            onClickNext={increment}
+          />
+        </div>
+        <div>
+          {loading ? (
+            <div>Cargando</div>
+          ) : (
+            <ul>
+              {characters.results.map((character: CharactersProps) => (
+                <li key={`${character.id}-${character.name}`}>
+                  <Link href={`/characters/${character.id}`}>
+                    <a>{character.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </Layout>
   )
 }
 
 export default Characters
+
+// export async function getStaticProps() {
+//   const { data, loading } = await client.query({
+//     query: charactersQuery,
+//   })
+
+//   const { results, info } = !!data && data?.characters
+
+//   return {
+//     props: {
+//       results,
+//       info,
+//       loading,
+//     },
+//   }
+// }
