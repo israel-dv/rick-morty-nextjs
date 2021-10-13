515909f66c85e3b4b2e0e400f899dd30698b9124
israel-dv <se.israel.mc@gmail.com>
2021-10-13 14:52:49 -0500

    Fix details ui

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -76,7 +76,7 @@ const Characters: NextPage = () => {
       <div className="flex flex-col w-full h-full">
         <div className="flex w-full justify-between items-center">
           <div className="flex flex-col">
-            <span className="text-3xl text-white font-bold font-comfortaa">
+            <span className="text-xl sm:text-3xl text-white font-bold font-comfortaa">
               {ROUTES.characters.label}
             </span>
             <span className="text-sm text-white font-comfortaa">
